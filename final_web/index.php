2e647ed888aedfdc989bbf6bde2edd0c2d9c047a
Ian Hibbert <ian.hibbert.a@gmail.com>
2015-08-11 12:15:36 -0400

    Complete Redesign
    Changed how the layout of the website is.

--- final_web/index.php
+++ final_web/index.php
@@ -49,8 +49,6 @@
 	}
 	
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -69,6 +67,8 @@
     <!--Lightbox-->
     <link href="css/lightbox.css" rel="stylesheet">
     
+    <!--Slideshow-->
+    <link href="css/slideshow.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -78,148 +78,138 @@
     <![endif]-->
   </head>
   
-  <body data-spy="scroll" data-target=".navbar-collapse">
-  
-  <!----------------Navbar---------------->
-  	<div class="navbar navbar-default">
-  		<div class="container">
-  			<div class = "navbar-header">
-	  			
-				<a href="#home" class="navbar-brand"> Dome Renovations</a> 
-				
-  				  				
-  				<button type="button" class ="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-  				
-  				<span class="sr-only">Toggle navigation</span>
-  				
-  				<span class="icon-bar"></span>
-  				<span class="icon-bar"></span>
-  				<span class="icon-bar"></span>		
-  			</div>
-  		</div>
-  	</div>
-  <!---------------End of Navbar---------->
-  
+  <body>
+   
 	<div class="container">
-		<div class="row" id="mainPage">
-			<div class="col-md-6" id="aboutUs">
-				<h1>About Us!</h1>
-				<p class="lead"> Brian Hibbert</p>
-				<p>
-					<!-- Trigger the modal with a button -->
-					<button type="button" class="btn btn-info" data-toggle="modal" data-target="#emailForm">Email Me</button>
-  				</p>
-			</div>
-			
-			<!---------------Renovations Gallery--------------->
-            <div class="col-md-6" id="renovationsGallery">
-                <h1>Past Renovations</h1>
-            
-					
-	            <div class="col-lg-6 col-md-3 col-xs-6 thumb">
-	                <a class="thumbnail" data-title="before-after-Bathroom" data-lightbox="Renovations" href="images/renovations/before-after-bathroom.jpg">
-	                    <img class="img-responsive img-rounded" src="images/thumbnails/before-after-bathroom-thumb.jpg" alt="">
-	                </a>
-	            </div>
-	            
-	             <div class="col-lg-6 col-md-3 col-xs-6 thumb">
-	                <a class="thumbnail" data-title="before-after-Bathroom" data-lightbox="Renovations" href="images/renovations/before-after-shower.jpg">
-	                    <img class="img-responsive img-rounded" src="images/thumbnails/before-after-shower-thumb.jpg" alt="">
-	                </a>
-	            </div>
-	            
-	             <div class="col-lg-6 col-md-3 col-xs-6 thumb">
-	                <a class="thumbnail" data-title="before-after-Bathroom" data-lightbox="Renovations" href="images/renovations/before-after-backdoor.jpg.jpg">
-	                    <img class="img-responsive img-rounded" src="images/thumbnails/before-after-backdoor-thumb.jpg" alt="">
-	                </a>
-	            </div>
-	            
-	            <div class="col-lg-6 col-md-3 col-xs-6 thumb">
-	                <a class="thumbnail" data-title="before-after-Bathroom" data-lightbox="Renovations" href="images/renovations/before-after-sidewalk.jpg">
-	                    <img class="img-responsive img-rounded" src="images/thumbnails/before-after-sidewalk-thumb.jpg" alt="">
-	                </a>
-	            </div>  
-            </div>           
-		
-		  <!--------------End of Renovations Gallery---->
+		<!--Brand Name-->
+		<div id="brandName" class="center">
+			<h1>Dome Renovations</h1>
+			<p class="lead" id="tagLine">Meticulous Renovations</p>
 		</div>
-	</div>
-  
-  
-	<!-- Modal -->
-	<div id="emailForm" class="modal fade" role="dialog">
-	  <div class="modal-dialog">
-	
-	    <!-- Modal content-->
-	    <div class="modal-content">
-	      <div class="modal-header">
-	        <button type="button" class="close" data-dismiss="modal">&times;</button>
-	        <h4 class="modal-title">Email Form</h4>
-	      </div>
-	    
-	      <div class="modal-dialog">
-		  <!--Email Form-->
-			<div class="col-md-6 col-md-offset-3">
-				<?php echo $result;?>
-				<form method= "post" role="form">
-					<div class="form-group">
-					<label for="name">Name:</label>
-					<input type="text" class="form-control" name="name" placeholder="Your Name" value="<?php echo $_POST['name'];?>">
-					</div>
+		
+		<!--Slidshow-->
+		<div class="bss-slides">  
+		    <figure>
+		        <img src="images/renovations/before-after-sidewalk.jpg" width="100%" />
+		        <figcaption>Before and After Backdoor</figcaption> 
+		    </figure>
+		    
+		    <figure>
+		        <img src="images/renovations/before-after-bathroom.jpg" width="100%" />
+		        <figcaption>Before and After Backdoor</figcaption> 
+		    </figure>
+		    
+			<figure>
+		        <img src="images/renovations/before-after-shower.jpg" width="100%" />
+		        <figcaption>Before and After Backdoor</figcaption> 
+		    </figure>
+		    
+		    <figure>
+		        <img src="images/renovations/before-after-backdoor.jpg" width="100%" />
+		        <figcaption>Before and After Backdoor</figcaption> 
+		    </figure>
+		    <!-- more figures here as needed -->
+		</div>
+		
+		
+		<!--Services-->
+		<row>
+		<!--Info-->
+			<div id="info">
+								
+				<section id="services" >
+					<p class="lead center"> Finest Renovations in the state of Delaware
+					<ul>
+						<li class="bottom-border">Bathrooms</li>
+						<li class="bottom-border">Kitchens</li>
+						<li class="bottom-border">Basements</li>
+						<li class="bottom-border" >Decks</li>
+					</ul>
 					
-					<div class="form-group">
-					<label for="email">Email address:</label>
-					<input type="email" class="form-control" name="email" placeholder="Your email address" value="<?php echo $_POST['email'];?>">
-					</div>
+					<div class="clear"></div>
 					
-					<div class="form-group">
-					<label for="Comment">Message:</label>
-					<textarea row="5" class="form-control" name="msg" placeholder="Hi my name is blank... Can you help me with this?" value="<?php echo $_POST['msg'];?>">
-					</textarea>
-					</div>
-					
-					<input name ="submit" class="btn btn-success" type="submit" value="Contact Me!"/>
-				</form>
-			</div>
-			<!--End of Email Form-->
-	      </div>
+					<ul>
+						<li>Security Camera Installions</li>
+						<li>Alarm Installions</li>
+					</ul>
+				</section>
+				
+				<h2>Brian Hibbert</h2>				
+				<div id="contactInfo">
+					<ul>
+						<li><span class="glyphicon glyphicon-earphone"></span> 973-204-8109</li>
+						<li><span class="glyphicon glyphicon-envelope"></span> contact@domerenovations.com</li>
+						<li><button type="button" id="message-button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#emailForm">Message Me!</button></li>
+					</ul>
 
-	  </div>
-	</div>
+				</div>
+			</div>
+		</row>
+		
+		<div class="clear"></div>
+
+			<!-- Email -->
+			<div id="emailForm" class="modal fade" role="dialog">
+			  <div class="modal-dialog">
+			    <!-- Modal content-->
+			    <div class="modal-content">
+			      <div class="modal-header">
+			        <button type="button" class="close" data-dismiss="modal">&times;</button>
+			        <h4 class="modal-title">Email Form</h4>
+			      </div>
+			    
+			      <div class="modal-body">
+				  <!--Email Form-->
+					
+				  	<?php echo $result;?>
+					<form method= "post" role="form">
+						<div class="form-group">
+							<label for="name">Name:</label>
+							<input type="text" class="form-control" name="name" placeholder="Your Name" value="<?php echo $_POST['name'];?>">
+						</div>
+						
+						<div class="form-group">
+							<label for="email">Email address:</label>
+							<input type="email" class="form-control" name="email" placeholder="Your email address" value="<?php echo $_POST['email'];?>">
+						</div>
+						
+						<div class="form-group">
+							<label for="Comment">Message:</label>
+							<textarea row="5" class="form-control" name="msg" placeholder="Hi my name is blank... Can you help me with this?" value="<?php echo $_POST['msg'];?>">
+							</textarea>
+							
+							
+							<input name ="submit" class="btn btn-success" id="contact-button" type="submit" value="Contact Me!"/>
+						</div>
+					</form>
+								
+					<div class="modal-footer">
+				    </div>	
+			    </div>
+		
+			  </div>
+			</div>
 
  
-	<!-- Footer -->
-	<footer>
-		<div class="row">
-		    <div class="col-lg-12">
-		        <p>Copyright &copy; Dome Renovations 2015</p>
-		    </div>
-		</div>
-	</footer>
+		<!-- Footer -->
+		<footer>
+			<div class="row">
+			    <div class="col-lg-12 footerInfo">
+			        <p id="copyright">Copyright &copy; Dome Renovations 2015</p>
+			    </div>
+			</div>
+		</footer>
+	</div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-    
-    <!--Lightbox.js-->
-    <script src="js/lightbox.min.js" type="text/javascript"> </script>
-	    
-	<!--Slidy-->
-	<script src="js/cssslidy.js" type="text/javascript"> </script>
-    <script>
-		cssSlidy({
-			timeOnSlide:2,
-		});
-		
-	</script>
-    
-    
-    <script>
-    var window_height = $(window).height();
-    	//$("#topRow h1").css("margin-top",window_height/2);
-    	$(".contentContainer").css("min-height",window_height);
-    </script>
-    
+  	
+  	<!-- Slideshow-->
+	<script src="js/slideshow.min.js"></script>  
+	<script>
+	  makeBSS('.bss-slides');
+	</script>  
   </body>
 </html>
